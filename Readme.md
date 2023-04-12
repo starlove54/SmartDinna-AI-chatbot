82c8d165b6e2b58ab9366836a6a82cae53832cbc
Anubhav Negi <anubhavnegi54@gmail.com>
2023-04-12 21:21:49 +0530

    update

---
+++ Readme.md
@@ -0,0 +1,19 @@
+
+![Alt text](client/assets/banner.png)
+## Introduction
+
+Smart Dinna is a AI powered chatbot web application that uses OpenAI's GPT-3 API to generate responses based on user prompts.
+
+## Technologies 
+
+-   Node.js
+-   Vue.js
+-   OpenAI API
+-   Express
+-   Nodemon
+-   Cors
+-   Dotenv
+
+## License
+
+Smart Dinna is licensed under the MIT license.
--- server/server.js
+++ server/server.js
@@ -2,23 +2,17 @@ import express from "express"
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from "openai"
-
-
 dotenv.config();
-
-console.log(process.env.OPENAI_API_KEY)
-
+// console.log(process.env.OPENAI_API_KEY)
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new  OpenAIApi(configuration);
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.get('/', async (req,res) => {
     res.status(200).send({
         message: 'Hello from SmartDinna',
